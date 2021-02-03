cf3c4c6151425a0f0cd6e7fc66df21b916db1c8b
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-02-03 16:57:41 +0200

    Initial commit

---
+++ src/test/java/com/github/theopechli/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.theopechli.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

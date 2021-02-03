c86fb9d594b1f443ecc6d9ac490d0d7cff4352b6
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-02-03 17:08:28 +0200

    Add function to fetch COVID-19 data

--- src/main/java/com/github/theopechli/coronavirustracker/services/CoronavirusDataService.java
+++ src/main/java/com/github/theopechli/coronavirustracker/services/CoronavirusDataService.java
@@ -1,4 +1,26 @@
 package com.github.theopechli.coronavirustracker.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
 public class CoronavirusDataService {
+
+    private final static String COVID_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    @PostConstruct
+    public void fetchCovidData() throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(COVID_DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+    }
+
 }
